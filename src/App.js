1d4ef53ba5a992c2e9b1a325e3ec0a3ce05e0797
Luis Antonio Peña Murillo <luis.pena@ironbit.com.mx>
2018-09-05 10:54:01 -0500

    Empiezo a dar forma al login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
 import './App.css';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="title" color="inherit">
-            Claro Connect
-          </Typography>
-        </Toolbar>
-      </AppBar>
+        Contenedor limpio
       </div>
     );
   }
