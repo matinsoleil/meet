cb7d71145764edd0953b9099f7e9303490031a25
Luis Antonio Peña Murillo <luis.pena@ironbit.com.mx>
2018-09-10 18:29:14 -0500

    Agrego la parte del usuario que esta logeado en la aplicación

--- src/components/chat/GeneralDataUser.js
+++ src/components/chat/GeneralDataUser.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const GeneralDataUser = () => {
+const GeneralDataUser = ({imgUser}) => {
     return (
         <div className="main-chat-general-data-user">
             <div className="icon-user">
+            <img src={imgUser} height="42" width="42" />
             </div>
             <div className="boton-action-user">
             </div>
