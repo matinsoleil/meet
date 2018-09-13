4a2a775d9c0a857cae9f4f36b081a0153203fc9d
Luis Antonio Peña Murillo <luis.pena@ironbit.com.mx>
2018-09-13 10:20:15 -0500

    Agrego la parte del ajax para la conversacion que se tiene con el usuario

--- src/components/chat/ChatGeneralConversationContact.js
+++ src/components/chat/ChatGeneralConversationContact.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import MessageReceiver from './MessageReceiver';
-import MessageTransmitter from './MessageTransmitter';
+// import MessageTransmitter from './MessageTransmitter';
 
 const ChatGeneralConversationContact = ({ chat }) => {
+    console.log(chat);
+    
     return (
         <div className="main-chat-history-conversation-contact">
             {chat.map(c => {
-                if (c.userSend === "1") {
-                    return (<MessageReceiver key={c.id} 
-                                             message={c.message} 
-                                             hour={c.hour}/>)
-                } else {
-                    return (<MessageTransmitter key={c.id}
-                                                message={c.message} 
-                                                hour={c.hour} />)
-                }
+                return (<MessageReceiver key={c.id}
+                    message={c.message}
+                    hour={c.hour} />)
             })}
         </div>
     );
--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -15,93 +15,6 @@ import { getContact } from './../selectors/contact'
 import { getUser } from './../selectors/user'
 import { getConversation } from '../selectors/conversation';
 
-const conversationOfContact = [{
-    "id": "1",
-    "userSend": "1",
-    "userGet": "2",
-    "message": "message",
-    "hour": "10:01",
-    "status": "1"
-}, {
-    "id": "2",
-    "userSend": "2",
-    "userGet": "1",
-    "message": "message",
-    "hour": "10:02",
-    "status": "1"
-}, {
-    "id": "3",
-    "userSend": "1",
-    "userGet": "2",
-    "message": "message",
-    "hour": "10:03",
-    "status": "1"
-}, {
-    "id": "4",
-    "userSend": "2",
-    "userGet": "1",
-    "message": "message",
-    "hour": "10:04",
-    "status": "1"
-}, {
-    "id": "5",
-    "userSend": "1",
-    "userGet": "2",
-    "message": "message",
-    "hour": "10:05",
-    "status": "1"
-}, {
-    "id": "6",
-    "userSend": "2",
-    "userGet": "1",
-    "message": "message",
-    "hour": "10:06",
-    "status": "1"
-}, {
-    "id": "7",
-    "userSend": "1",
-    "userGet": "2",
-    "message": "message",
-    "hour": "10:07",
-    "status": "1"
-}, {
-    "id": "8",
-    "userSend": "2",
-    "userGet": "1",
-    "message": "message",
-    "hour": "10:08",
-    "status": "1"
-}, {
-    "id": "9",
-    "userSend": "1",
-    "userGet": "2",
-    "message": "message",
-    "hour": "10:09",
-    "status": "1"
-}, {
-    "id": "10",
-    "userSend": "2",
-    "userGet": "1",
-    "message": "message",
-    "hour": "10:10",
-    "status": "1"
-}, {
-    "id": "11",
-    "userSend": "1",
-    "userGet": "2",
-    "message": "message",
-    "hour": "10:11",
-    "status": "1"
-},
-{
-    "id": "12",
-    "userSend": "2",
-    "userGet": "1",
-    "message": "message",
-    "hour": "10:12",
-    "status": "1"
-}]
-
 class ChatContainer extends Component {
     componentDidMount() {
         this.props.fetchContacts();
@@ -125,7 +38,7 @@ class ChatContainer extends Component {
                 <div className="main-chat-body">
                     <ListGeneralContacts contacts={contacts} />
                     <div className="main-chat-general-conversation-contact">
-                        <ChatGeneralConversationContact chat={conversationOfContact} />
+                        <ChatGeneralConversationContact chat={conversation} />
                         <AreaSendMessage />
                     </div>
                 </div>
@@ -147,7 +60,8 @@ class ChatContainer extends Component {
 ChatContainer.defaultProps = {
     contacts: [],
     user: [],
-    contact: []
+    contact: [],
+    conversation:[]
 }
 const mapStateToProps = (state, props) => {
     return {
