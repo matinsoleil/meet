b42171ed42c3a4e2f3f5e325d6fb88ce118ac1f1
Adrian Romero <adrianromero@customer-187-174-218-184.uninet-ide.com.mx>
2018-10-17 10:28:52 -0500

    adrian styles

--- src/components/chat/contactsSection/GeneralContactData.js
+++ src/components/chat/contactsSection/GeneralContactData.js
@@ -18,8 +18,8 @@ class GeneralContactData extends Component {
                             <div className="inner-circle circle">&nbsp;</div>
                         </div>
                     </div>
-                    <div className="name-contact">{this.props.contact.name}</div>
-                    <div className="day-last-message"><p>{this.props.contact.dayLastMessage}</p></div>
+                    <div className="name-contact"><p className="text-contact">{this.props.contact.name}</p></div>
+                    <div className="day-last-message"><p className="day-last">{this.props.contact.dayLastMessage}</p></div>
                     <div className="last-message">{this.props.contact.lastMessage}</div>
                     <div className="count-message">
                         <div className="circle-count-message"><p>{this.props.contact.countMessage}</p>
