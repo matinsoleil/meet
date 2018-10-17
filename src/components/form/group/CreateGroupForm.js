5cced698f29a1f59fd9cd4c0f9732f0c0f2293bf
Luis Antonio Peña Murillo <luis.pena@ironbit.com.mx>
2018-10-17 17:01:12 -0500

    Agrego la parte de las validaciones en la creación de los grupos

--- src/components/form/group/CreateGroupForm.js
+++ src/components/form/group/CreateGroupForm.js
@@ -2,23 +2,59 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import './CreateGroupForm.scss'
 
+const validate = values => {
+  const errors = {}
+  if (!values.nameGroup) {
+    errors.nameGroup = 'Debe colocar un nombre al grupo'
+  } else if (values.nameGroup.length < 3) {
+    errors.nameGroup = 'El nombre del grupo debe ser mayor a 3 caracteres'
+  } else if (values.nameGroup.length > 15) {
+    errors.nameGroup = 'El nombre del grupo debe ser menor a 15 caracteres'
+  }
+  return errors
+}
+
+const warn = values => {
+  const warnings = {}
+  if (values.nameGroup === "19") {
+    warnings.nameGroup = 'Warning'
+  }
+  return warnings
+}
+
+const renderField = ({
+  input,
+  label,
+  type,
+  placeholder,
+  meta: { touched, error, warning }
+}) => (
+    <div>
+      <label className="title-name-group">{label}</label>
+      <div>
+        <input {...input} placeholder={placeholder} type={type} className="input-name-group" />
+        {touched &&
+          ((error && <p><span className="error">{error}</span></p>) ||
+            (warning && <p><span className="warning">{warning}</span></p>))}
+      </div>
+    </div>
+  )
+
 let CreateGroupForm = props => {
-  const { handleSubmit, closeWindow } = props
+  const { handleSubmit, closeWindow, submitting } = props
   return (
     <form onSubmit={handleSubmit}>
-      <div>
-        <label className="title-name-group">Escribe el nombre del grupo</label>
-        <Field name="nameGroup" component="input" type="text" className="input-name-group" />
-      </div>
-      <button className="from-create-group-btn" onClick={this.createGroup}>Agregar</button>
+      <Field name="nameGroup" label="Escribe el nombre del grupo" placeholder="Nombre del grupo" type="input" type="text" component={renderField} />
+      <button className="from-create-group-btn" type="submit" >Agregar</button>
       <button className="from-create-group-btn" onClick={closeWindow}>Close</button>
     </form>
   )
 }
 
 CreateGroupForm = reduxForm({
-  // a unique name for the form
-  form: 'contact'
+  form: 'group',
+  validate,
+  warn
 })(CreateGroupForm)
 
 export default CreateGroupForm
