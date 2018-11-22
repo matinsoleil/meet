840a2d39b6d23d9c0329b70c7fac136e972e7c40
Daniel Noblecias <daniellopez@ctinmx.mx>
2018-11-22 10:49:28 -0600

    add: changes in RightSection

--- src/components/chat/rightSection/RightSection.js
+++ src/components/chat/rightSection/RightSection.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import RightSideContactList from './RightSideContactList/RightSideContactList';
 import { connect } from 'react-redux';
 import { createNewGroup } from '../../../redux/actions/groups/groups';
+import { toggleRightSide, Type } from '../../../redux/actions/rightSection/rightSection';
 import hideSectionRight from '../../../redux/actions/rightSection/hideSectionRight';
 import './RightSection.scss';
-
 class RightSection extends Component {
 
     constructor(props) {
@@ -13,31 +13,35 @@ class RightSection extends Component {
             View: undefined,
         }
     }
-
+    currentAction = () => {
+        const { type } = this.props;
+        return (type === Type.CREATE_GROUP) ? this.props.createNewGroup
+            : (type === Type.FORWARD_MESSAGE) ? null
+                : (type === Type.CREATE_DIFFUSION_GROUP) && null;
+    }
     componentWillMount() {
-        (this.props.type)&&import('./'+this.props.type).then(e=>{
+        (this.props.type) && import('./' + this.props.type).then(e => {
             this.setState({
                 View: e.default
             });
         });
     }
-
     render() {
         const { View } = this.state;
         return (
             <div className="right-section-container">
-                {(View) && <View />}
+                {(View) && <View title={this.props.title} action={this.currentAction()} />}
             </div>
         );
     }
 
 }
-export default connect(state=>({
+export default connect(state => ({
     title: state.rightSection.title,
     type: state.rightSection.type,
 }), dispatch => ({
     createNewGroup: (newConversation, newGroupElement) => {
         dispatch(createNewGroup(newConversation, newGroupElement));
-        dispatch(hideSectionRight());
+        dispatch(toggleRightSide(undefined,undefined));
     },
 }))(RightSection);
