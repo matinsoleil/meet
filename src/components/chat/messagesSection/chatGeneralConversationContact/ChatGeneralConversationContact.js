0db39a60a81e4d788e0845d2e8b0a6aa9708df97
Luis Antonio Peña Murillo <luis.pena@ironbit.com.mx>
2018-12-04 15:36:42 -0600

    Quito la validacion del messages

--- src/components/chat/messagesSection/chatGeneralConversationContact/ChatGeneralConversationContact.js
+++ src/components/chat/messagesSection/chatGeneralConversationContact/ChatGeneralConversationContact.js
@@ -11,25 +11,22 @@ class ChatGeneralConversationContact extends Component {
             <div id='#main-chat-feed' style={{ backgroundImage: `url(${this.props.background})` }} className="main-chat-history-conversation-contact">
                 <div className="initial"></div>
                 {
-                    (this.props.messages) ?
-                        this.props.messages.map(c => {
-                            const TypeMessageControl = c.sender.toString() === this.props.user.idUser.toString() ? 'message-in' : 'message-out';
-                            const tailType = c.sender.toString() === this.props.user.idUser.toString() ? 'tail-out' : 'tail-in';
-                            const tail = (c.sender.toString() === this.props.user.idUser.toString()) ? this.props.out_tail : this.props.in_tail;
-                            return <Message
-                                key={c.id}
-                                messageObject={c}
-                                tail={tail}
-                                type={TypeMessageControl}
-                                tailType={tailType}
-                                user_icon={this.props.conversation.image || Images.avatar}
-                                contacts={[]}
-                                // contacts={props.contacts}
-                                conversationId={this.props.messages.conversationID}
-                            />
-                        })
-                        :
-                        null
+                    this.props.messages.map(c => {
+                        const TypeMessageControl = c.sender.toString() === this.props.user.idUser.toString() ? 'message-in' : 'message-out';
+                        const tailType = c.sender.toString() === this.props.user.idUser.toString() ? 'tail-out' : 'tail-in';
+                        const tail = (c.sender.toString() === this.props.user.idUser.toString()) ? this.props.out_tail : this.props.in_tail;
+                        return <Message
+                            key={c.id}
+                            messageObject={c}
+                            tail={tail}
+                            type={TypeMessageControl}
+                            tailType={tailType}
+                            user_icon={this.props.conversation.image || Images.avatar}
+                            contacts={[]}
+                            // contacts={props.contacts}
+                            conversationId={this.props.messages.conversationID}
+                        />
+                    })
                 }
             </div>
         )
