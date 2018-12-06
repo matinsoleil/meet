e886efc7317a127403ed1796d26ed7007553490f
Luis Antonio Peña Murillo <luis.pena@ironbit.com.mx>
2018-12-06 12:19:05 -0600

    Hago la modificación de los mensajes

--- src/lib/helper/controlMenuMessage.js
+++ src/lib/helper/controlMenuMessage.js
@@ -1,13 +1,16 @@
 import { store } from '../../redux/store/index';
 import { configureModalConfirm } from '../../redux/actions/views/modalConfirm';
 import { updateMessage } from '../../redux/actions/messages/messages';
+import { messageSelected } from '../../redux/actions/messagesOptions/messagesOptions';
+
 
 class ControlMenuMessageHelper {
 
     static toggleReplyMessage(conversation, message) {
         return {
             text: store.getState().country.translator.t('REPLY'),
-            clickHandler: () => null
+            // clickHandler: () => alert(" ideas ")
+            clickHandler: () => store.dispatch(messageSelected(message.id, true))
         }
     }
 
